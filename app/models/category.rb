596f6d8893ae8c9c3e3621012548ebc77c66250b
Kevin Poole <kevitron@hotmail.com>
2025-02-05 13:23:41 -0500

    added s3 bucket

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
     has_one_attached :image
     has_many :products
+
+    before_destroy :delete_image_from_s3, prepend: true
+
+  private
+
+  def delete_image_from_s3
+    image.purge if image.attached?
+  end
 end
