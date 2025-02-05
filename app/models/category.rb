f594ab1097250e0055feac49334e10ff34923041
Kevin Poole <kevitron@hotmail.com>
2025-02-05 13:07:09 -0500

    added s3 bucket

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,11 @@ class Category < ApplicationRecord
     has_one_attached :image
     has_many :products
 
-    attachable.analyze_later false 
+    after_save :skip_analysis
+
+  private
+
+  def skip_analysis
+    image.analyze_later = false if image.attached?
+  end
 end
