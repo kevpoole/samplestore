e2efc6d8b57bf46f1a92b1e81da0738249f57dc5
Kevin Poole <kevitron@hotmail.com>
2025-02-05 05:16:09 -0500

    stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,9 @@
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+if Rails.env.production?
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    Stripe.publishable_key = ENV['STRIPE_PUBLISHABLE_KEY']
+  else
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+    Stripe.publishable_key = Rails.application.credentials.dig(:stripe, :publishable_key)
+  end
